0b6fd1df79a4aa0376e8208a59cf017f30e7ce9c
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-06-01 17:24:16 -0300

    new files

---
+++ dfdfgs/dfdfgs/Program.cs
@@ -0,0 +1,30 @@
+﻿using dfdfgs;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int yearOld = int.Parse(Console.ReadLine());
+            Person pessoa1 = new Person(name, yearOld);
+
+            string name2 = Console.ReadLine();
+            int yearOld2 = int.Parse(Console.ReadLine());
+            Person pessoa2 = new Person(name2, yearOld2);
+
+            if (pessoa1.YeOld > pessoa2.YeOld)
+            {
+                Console.WriteLine("mais velho: " + pessoa1.Name);
+            }
+            else
+            {
+                Console.WriteLine("mais velho: " + pessoa2.Name); ;
+            }
+
+
+        }
+    }
+
+}
